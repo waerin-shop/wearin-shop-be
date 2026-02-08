3d93ed708ea2adf61884a411fe9130a0b3657b34
unknown <rakasantang22@gmail.com>
2026-02-08 21:15:03 +0700

    add allowed origins

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ const PORT = process.env.PORT || 5000;
 // --- KONFIGURASI CORS ---
 const allowedOrigins = [
    "http://localhost:3000",
+   "https://wearin-shop-fe-2eo9.vercel.app",
 ];
 
 app.use(cors({
